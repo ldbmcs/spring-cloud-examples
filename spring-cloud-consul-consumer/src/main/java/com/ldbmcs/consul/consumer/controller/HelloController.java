badbeb6467490c524e0a8bd18522b20d3102ecc9
ldbmcs <ldbmcs@gmail.com>
2020-08-05 09:45:17 +0800

    新增Logstash配置

--- spring-cloud-consul-consumer/src/main/java/com/ldbmcs/consul/consumer/controller/HelloController.java
+++ spring-cloud-consul-consumer/src/main/java/com/ldbmcs/consul/consumer/controller/HelloController.java
@@ -1,6 +1,8 @@
 package com.ldbmcs.consul.consumer.controller;
 
 import com.ldbmcs.consul.consumer.feign.HelloService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,8 +14,11 @@ public class HelloController {
     @Autowired
     HelloService helloService;
 
+    Logger logger = LoggerFactory.getLogger(HelloController.class);
+
     @RequestMapping("/hello")
     public String index(@RequestParam String name) {
+        logger.info("调用spring-cloud-consul-producer服务");
         return helloService.hello(name);
     }
 
--- spring-cloud-consul-producer/src/main/java/com/ldbmcs/consul/producer/controller/HelloController.java
+++ spring-cloud-consul-producer/src/main/java/com/ldbmcs/consul/producer/controller/HelloController.java
@@ -1,5 +1,7 @@
 package com.ldbmcs.consul.producer.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    Logger logger = LoggerFactory.getLogger(HelloController.class);
+
     @RequestMapping("/hello")
     public String index(@RequestParam String name) {
+        logger.info("接收spring-cloud-consul-consumer服务请求");
         return "hello " + name + "，this is first messge";
     }
 
